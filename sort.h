81e90ac6d7c386c0ebdf5128ab2ddec0b38c9b38
Triad01 <luckypee01@gmail.com>
2023-12-14 20:32:37 +0100

    implemented bubble sort for task 0

--- sort.h
+++ sort.h
@@ -2,6 +2,8 @@
 #define SORT_H
 #include <stdio.h>
 #include <stdlib.h>
+#include <string.h>
+#include <stddef.h>
 /**
  * struct listint_s - Doubly linked list node
  *
@@ -15,7 +17,7 @@ typedef struct listint_s
 	struct listint_s *prev;
 	struct listint_s *next;
 } listint_t;
-
+void bubble_sort(int *array, size_t size);
 void print_array(const int *array, size_t size);
 void print_list(const listint_t *list);
 #endif
