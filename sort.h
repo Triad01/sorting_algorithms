34f81edcb4c6f5e5845c967fda7435be82ff89bb
Genius-Excel <geniusexcel9@gmail.com>
2023-12-19 15:27:54 -0800

    A new prototype function added.

--- sort.h
+++ sort.h
@@ -25,4 +25,5 @@ void selection_sort(int *array, size_t size);
 int partition(int *array, int lower, int higher, int size);
 void set_pivot(int *array, int low, int high, int size);
 void quick_sort(int *array, size_t size);
+void shell_sort(int *array, size_t size);
 #endif
